a32ef465b692fd91659051aa22f6f84e85ab8188
enesments <enesments@gmail.com>
2023-03-08 01:34:20 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfaceAbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
